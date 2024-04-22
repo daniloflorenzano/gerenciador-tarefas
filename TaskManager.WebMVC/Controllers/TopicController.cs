764f907d6ae12a6ab3de8e61710dd807079cb3a5
Danilo Maia Florenzano <daniloflorenzano1@outlook.com>
2024-04-21 23:33:25 -0300

    faz scaffold para criar controller e views de Topic

---
+++ TaskManager.WebMVC/Controllers/TopicController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+using TaskManager.Models.Models;
+
+namespace TaskMaganer.Controllers
+{
+    public class TopicController : Controller
+    {
+        private readonly TaskManagerContext _context;
+
+        public TopicController(TaskManagerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Topic
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Topics.ToListAsync());
+        }
+
+        // GET: Topic/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topics
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return View(topic);
+        }
+
+        // GET: Topic/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Topic/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Color")] Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(topic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(topic);
+        }
+
+        // GET: Topic/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topics.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+            return View(topic);
+        }
+
+        // POST: Topic/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Color")] Topic topic)
+        {
+            if (id != topic.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(topic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TopicExists(topic.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(topic);
+        }
+
+        // GET: Topic/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topics
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return View(topic);
+        }
+
+        // POST: Topic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var topic = await _context.Topics.FindAsync(id);
+            if (topic != null)
+            {
+                _context.Topics.Remove(topic);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TopicExists(int id)
+        {
+            return _context.Topics.Any(e => e.Id == id);
+        }
+    }
+}
--- TaskManager.WebMVC/Program.cs
+++ TaskManager.WebMVC/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<TaskManagerContext>();
 
 var app = builder.Build();
 
