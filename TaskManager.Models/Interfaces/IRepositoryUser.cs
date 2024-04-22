72ffbebd173c652d3e381276cca36ef2c03bd499
João Sampaio <jpsampaio80@gmail.com>
2024-04-22 15:21:47 -0300

    Adição das view's de usuários e adição da pasta Interfaces paraorganizar e separar Interfaces de Repositórios

---
+++ TaskManager.Models/Interfaces/IRepositoryUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Models.Models;
+
+namespace TaskManager.Models.Interfaces
+{
+    public interface IRepositoryUser : IRepositoryBase<User, TaskManagerContext>
+    {
+    }
+}
