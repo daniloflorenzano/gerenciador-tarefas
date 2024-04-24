1caf896a4d77805a6295d45fc2853c175f4e04c4
Danilo Maia Florenzano <daniloflorenzano1@outlook.com>
2024-04-23 21:19:49 -0300

    melhora paleta de cores disponiveis para topicos

--- TaskManager.Models/Partials/TopicExtension.cs
+++ TaskManager.Models/Partials/TopicExtension.cs
@@ -21,11 +21,11 @@ public partial class Topic
 
         return color switch
         {
-            TopicColorOptionsEnum.Azul => ColorTranslator.ToHtml(System.Drawing.Color.Blue),
-            TopicColorOptionsEnum.Verde => ColorTranslator.ToHtml(System.Drawing.Color.Green),
-            TopicColorOptionsEnum.Vermelho => ColorTranslator.ToHtml(System.Drawing.Color.Red),
+            TopicColorOptionsEnum.Azul => ColorTranslator.ToHtml(System.Drawing.Color.DodgerBlue),
+            TopicColorOptionsEnum.Verde => ColorTranslator.ToHtml(System.Drawing.Color.LawnGreen),
+            TopicColorOptionsEnum.Vermelho => ColorTranslator.ToHtml(System.Drawing.Color.IndianRed),
             TopicColorOptionsEnum.Amarelo => ColorTranslator.ToHtml(System.Drawing.Color.Yellow),
-            TopicColorOptionsEnum.Roxo => ColorTranslator.ToHtml(System.Drawing.Color.Purple),
+            TopicColorOptionsEnum.Roxo => ColorTranslator.ToHtml(System.Drawing.Color.MediumPurple),
             TopicColorOptionsEnum.Laranja => ColorTranslator.ToHtml(System.Drawing.Color.Orange),
             _ => throw new Exception("Código da cor não catalogado.")
         };
